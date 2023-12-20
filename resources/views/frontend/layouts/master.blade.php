c887125c8bf0fb507064b5baf1447f740db4ae90
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-12-20 18:01:59 +0100

    Added user login page

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,3 +1,4 @@
+@php use function Laravel\Prompts\error; @endphp
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,6 +19,7 @@
 
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css')}}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css')}}">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
 </head>
 
@@ -107,9 +109,20 @@
 <script src="{{ asset('frontend/js/wow.min.js')}}"></script>
 <!-- ex zoom js -->
 <script src="{{ asset('frontend/js/jquery.exzoom.js')}}"></script>
+<!-- toastr js -->
+<script src="{{ asset('frontend/js/toastr.min.js')}}"></script>
 
 <!--main/custom js-->
 <script src="{{ asset('frontend/js/main.js')}}"></script>
+
+<script>
+    toastr.options.progressBar = true;
+    @if ($errors->any())
+        @foreach($errors->all() as $error)
+            toastr.error("{{$error}}");
+        @endforeach
+    @endif
+</script>
 
 </body>
 
