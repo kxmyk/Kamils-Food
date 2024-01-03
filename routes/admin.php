6c45dfd07661e0e4d36db628a824e36ce0a0f619
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-01-03 19:16:26 +0100

    Admin data update feature

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    // Dashboard
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+    // Profile routes
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::put('/profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
 });
