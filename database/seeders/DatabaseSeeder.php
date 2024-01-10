abeb9757f5b2e64feb4a8ac1cd6f274285bbf385
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-01-10 21:04:28 +0100

    [FEATURE] Slider - created SliderFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Slider;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(UserSeeder::class);
+//        $this->call(UserSeeder::class);
+
+        Slider::factory(5)->create();
+
+
     }
 }
