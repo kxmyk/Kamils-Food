8ec2f018291942cdddcbc7ef7e5d90750bf7e9a9
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-01-26 17:31:23 +0100

    [FEATURE] Product - delete feature

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -8,8 +8,12 @@ use App\Http\Requests\Admin\ProductUpdateRequest;
 use App\Models\Category;
 use App\Models\Product;
 use App\Traits\FileUploadTrait;
+use Exception;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
@@ -18,7 +22,7 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(ProductDataTable $dataTable)
+    public function index(ProductDataTable $dataTable): View|JsonResponse
     {
         return $dataTable->render('admin.product.index');
     }
@@ -35,7 +39,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $product = new Product();
         $imagePath = $this->uploadImage($request, 'thumb_image');
@@ -83,7 +87,7 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(ProductUpdateRequest $request, string $id)
+    public function update(ProductUpdateRequest $request, string $id): RedirectResponse
     {
         $product = Product::findOrFail($id);
         $imagePath = $this->uploadImage($request, 'thumb_image');
@@ -111,8 +115,23 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): Response
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            $this->removeImage($product->thumb_image);
+            $product->delete();
+
+            return response([
+                'status' => 'success',
+                'message' => 'Deleted Successfully'
+            ]);
+        } catch (Exception $e) {
+
+            return response([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
