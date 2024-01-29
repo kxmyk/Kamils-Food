fea25a72a569dd826b837325db7137e8fd5d999a
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-01-29 20:15:39 +0100

    [FEATURE] Product Gallery - create feature

--- app/Http/Controllers/Admin/ProductGalleryController.php
+++ app/Http/Controllers/Admin/ProductGalleryController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductGallery;
+use App\Traits\FileUploadTrait;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ProductGalleryController extends Controller
 {
+    use FileUploadTrait;
+
     /**
      * Display a listing of the resource.
      */
-    public function index(): View
+    public function index(string $productId): View
     {
-        return view('admin.product.gallery.index');
+        return view('admin.product.gallery.index', compact('productId'));
     }
 
     /**
@@ -29,7 +33,22 @@ class ProductGalleryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'image' => ['required', 'image', 'max:3000'],
+            'product_id' => ['required', 'integer']
+        ]);
+
+        $imagePath = $this->uploadImage($request, 'image');
+
+        $gallery = new ProductGallery();
+        $gallery->product_id = $request->product_id;
+        $gallery->image = $imagePath;
+        $gallery->save();
+
+        toastr()->success('Created Successfully!');
+
+        return redirect()->back();
+
     }
 
     /**
