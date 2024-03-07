80896dfb62faeb9075847e469f13400b03035624
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-03-07 21:28:03 +0100

    [FEATURE] Coupons - creating migration and columns

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Coupon;
 use App\Models\Product;
 use App\Models\Slider;
 use App\Models\WhyChooseUs;
@@ -28,5 +30,6 @@ class DatabaseSeeder extends Seeder
         $this->call(WhyChooseUs::factory(3)->create());
         $this->call(CategorySeeder::class);
         Product::factory(10)->create();
+        Coupon::factory(3)->create();
     }
 }
