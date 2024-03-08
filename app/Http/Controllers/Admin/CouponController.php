adc8e8c0682a74f672e064617f6577f5ed5542e8
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-03-08 14:43:27 +0100

    [FEATURE] Coupons - update and delete feature

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CouponCreateRequest;
 use App\Models\Coupon;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class CouponController extends Controller
@@ -17,7 +17,7 @@ class CouponController extends Controller
      */
     public function index(CouponDataTable $couponDataTable)
     {
-        return $couponDataTable->render('admin.coupons.index');
+        return $couponDataTable->render('admin.coupon.index');
     }
 
     /**
@@ -25,7 +25,7 @@ class CouponController extends Controller
      */
     public function create(): View
     {
-        return view('admin.coupons.create');
+        return view('admin.coupon.create');
     }
 
     /**
@@ -60,24 +60,48 @@ class CouponController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
-        //
+        $coupon = Coupon::findOrFail($id);
+        return view('admin.coupon.edit', compact('coupon'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CouponCreateRequest $request, string $id): RedirectResponse
     {
-        //
+        $coupon = Coupon::findOrFail($id);
+        $coupon->name = $request->name;
+        $coupon->code = $request->code;
+        $coupon->quantity = $request->quantity;
+        $coupon->min_purchase_amount = $request->min_purchase_amount;
+        $coupon->expire_date = $request->expire_date;
+        $coupon->discount_type = $request->discount_type;
+        $coupon->discount = $request->discount;
+        $coupon->status = $request->status;
+        $coupon->save();
+
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.coupon.index');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): Response
     {
-        //
+        try {
+            Coupon::findOrFail($id)->delete();
+
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+
+        } catch (\Exception $e) {
+            return response(['status' => 'error', 'message' => 'something went wrong!']);
+        }
+
     }
 }
