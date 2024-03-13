0977e2677121d3a78884f91753e2b067bf5bb04d
Kamil Lukasiuk <kamilplgt@gmail.com>
2024-03-13 14:06:23 +0100

    [FEATURE] Customer Address - create feature

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -3,13 +3,36 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\AddressCreateRequest;
+use App\Models\Address;
+use App\Models\DeliveryArea;
 use Illuminate\Contracts\View\View;
-
 
 class DashboardController extends Controller
 {
-    function index(): View
+    public function index(): View
     {
-        return view('frontend.dashboard.index');
+        $deliveryAreas = DeliveryArea::where('status', 1)->get();
+        $userAddresses = Address::where('user_id', auth()->user()->id)->get();
+
+        return view('frontend.dashboard.index', compact('deliveryAreas', 'userAddresses'));
+    }
+
+    public function createAddress(AddressCreateRequest $request)
+    {
+        $address = new Address();
+        $address->user_id = auth()->user()->id;
+        $address->delivery_area_id = $request->area;
+        $address->first_name = $request->first_name;
+        $address->last_name = $request->last_name;
+        $address->email = $request->email;
+        $address->phone = $request->phone;
+        $address->address = $request->address;
+        $address->type = $request->type;
+        $address->save();
+
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.dashboard');
     }
 }
